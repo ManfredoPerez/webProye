cd9c9cd4eec5d0593d2fea2c6670ae6b2a35762d
Manfredo Pérez <perezmanfredo959@gmail.com>
2022-10-01 12:09:41 -0600

    Conexio

--- DESARROLLOWEB/modelos/conexion.php
+++ DESARROLLOWEB/modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=proyectoweb",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=bndzjq8rvgwtpb1ca0gd-mysql.services.clever-cloud.com;dbname=bndzjq8rvgwtpb1ca0gd",
+			            "uuechexzqjblh9nx",
+			            "AEA2LRJfhZfJcMiZzzhU");
 
 		$link->exec("set names utf8");
 
